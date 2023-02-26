6b2b1b0dddf62865d17260e64bf4bb18d4e05831
zaksalazar <zsalazar.tv@gmail.com>
2023-02-26 11:58:21 -0800

    styling cards

--- react-portfolio/src/components/Projectcard.jsx
+++ react-portfolio/src/components/Projectcard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Card = (props) => {
+const Projectcard = (props) => {
   return (
     <div
       id="portfolio"
@@ -19,7 +19,7 @@ const Card = (props) => {
         </p>
         <a
           href={props.link}
-          class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          class="bg-violet-500 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Github Repo
           <svg
@@ -60,4 +60,4 @@ const Card = (props) => {
   );
 };
 
-export default Card;
+export default Projectcard;
